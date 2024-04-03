bb1abc32c06b42399316597459c01bad9b0df75d
Felipe Avila <juanfe.avilat@uniagustiniana.edu.co>
2024-04-03 16:40:25 -0500

    Componente Ingresos Funcional

--- src/main/java/co/finanplus/api/controller/IngresoController.java
+++ src/main/java/co/finanplus/api/controller/IngresoController.java
@@ -1,6 +1,8 @@
 package co.finanplus.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import co.finanplus.api.domain.Ingresos.Ingreso;
@@ -15,15 +17,19 @@ public class IngresoController {
     @Autowired
     private IngresoRepository ingresoRepository;
 
+    // endpoint para obtener los ingresos de un usuario
     @GetMapping
-    public List<Ingreso> listIngresos(@PathVariable String usuarioID) {
-        return ingresoRepository.findByUsuarioID(usuarioID);
+    public ResponseEntity<List<Ingreso>> listIngresos(@PathVariable String usuarioID) {
+        List<Ingreso> ingresos = ingresoRepository.findByUsuarioID(usuarioID);
+        return new ResponseEntity<>(ingresos, HttpStatus.OK);
     }
 
+    // endpoint para agregar un ingreso a un usuario
     @PostMapping
-    public Ingreso addIngreso(@PathVariable String usuarioID, @RequestBody Ingreso ingreso) {
+    public ResponseEntity<Ingreso> addIngreso(@PathVariable String usuarioID, @RequestBody Ingreso ingreso) {
         ingreso.setUsuarioID(usuarioID);
-        return ingresoRepository.save(ingreso);
+        Ingreso savedIngreso = ingresoRepository.save(ingreso);
+        return new ResponseEntity<>(savedIngreso, HttpStatus.CREATED);
     }
 
     // Más métodos para actualizar y eliminar ingresos
