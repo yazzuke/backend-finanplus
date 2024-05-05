03cf78f7ab37321766544111a5f6bad09b98fea0
Felipe Avila <126629999+yazzuke@users.noreply.github.com>
2024-05-05 12:38:06 -0500

    modding endpoint usuarios

--- src/main/java/co/finanplus/api/controller/CorsConfiguration.java
+++ src/main/java/co/finanplus/api/controller/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5174")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") 
                         .allowedHeaders("*")
                         .allowCredentials(true)
--- src/main/java/co/finanplus/api/controller/UsuarioController.java
+++ src/main/java/co/finanplus/api/controller/UsuarioController.java
@@ -1,62 +1,75 @@
-package co.finanplus.api.controller;
+    package co.finanplus.api.controller;
 
-import java.time.LocalDate;
+    import java.time.LocalDate;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.http.ResponseEntity;
+    import org.springframework.web.bind.annotation.GetMapping;
+    import org.springframework.web.bind.annotation.PathVariable;
+    import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+    import org.springframework.web.bind.annotation.RestController;
 
-import co.finanplus.api.domain.usuarios.Usuario;
-import co.finanplus.api.domain.usuarios.UsuarioRepository;
-import co.finanplus.api.service.ResumenMensualService;
+    import co.finanplus.api.domain.usuarios.Usuario;
+    import co.finanplus.api.domain.usuarios.UsuarioRepository;
+    import co.finanplus.api.service.ResumenMensualService;
 
-@RestController
-public class UsuarioController {
+    @RestController
+    public class UsuarioController {
 
-    @Autowired
-    private UsuarioRepository usuarioRepository;
+        @Autowired
+        private UsuarioRepository usuarioRepository;
 
-    @Autowired
-    private ResumenMensualService resumenMensualService;
+        @Autowired
+        private ResumenMensualService resumenMensualService;
 
+        // endpoint para crear un usuario
     // endpoint para crear un usuario
- // endpoint para crear un usuario
-@PostMapping("/usuarios")
-public Usuario crearUsuario(@RequestBody Usuario usuario) {
-    Usuario nuevoUsuario = new Usuario();
-    nuevoUsuario.setId(usuario.getId());
-    nuevoUsuario.setNombre(usuario.getNombre());
-    nuevoUsuario.setEmail(usuario.getEmail());
-    nuevoUsuario.setPhotoUrl(usuario.getPhotoUrl());  
-    
-    Usuario usuarioGuardado = usuarioRepository.save(nuevoUsuario);
+    @PostMapping("/usuarios")
+    public Usuario crearUsuario(@RequestBody Usuario usuario) {
+        Usuario nuevoUsuario = new Usuario();
+        nuevoUsuario.setId(usuario.getId());
+        nuevoUsuario.setNombre(usuario.getNombre());
+        nuevoUsuario.setEmail(usuario.getEmail());
+        nuevoUsuario.setPhotoUrl(usuario.getPhotoUrl());  
+        
+        Usuario usuarioGuardado = usuarioRepository.save(nuevoUsuario);
 
-    // Crear un resumen mensual inicial después de registrar al usuario
-    LocalDate fechaInicio = LocalDate.now();
-    resumenMensualService.crearResumenInicial(usuarioGuardado.getId(), fechaInicio);
+        // Crear un resumen mensual inicial después de registrar al usuario
+        LocalDate fechaInicio = LocalDate.now();
+        resumenMensualService.crearResumenInicial(usuarioGuardado.getId(), fechaInicio);
 
-    return usuarioGuardado;
-}
+        return usuarioGuardado;
+    }
 
-    // endpoint para obtener un usuario por su id
-    @GetMapping("/usuarios/{userId}")
-    public Usuario obtenerUsuario(@PathVariable String userId) {
-        return usuarioRepository.findById(userId).orElse(null);
+        // endpoint para obtener un usuario por su id
+        @GetMapping("/usuarios/{userId}")
+        public Usuario obtenerUsuario(@PathVariable String userId) {
+            return usuarioRepository.findById(userId).orElse(null);
+        }
+
+
+        // endpoint para obtener un usuario por su email
+        @GetMapping("/usuarios/email/{email}")
+        public ResponseEntity<Usuario> obtenerUsuarioPorEmail(@PathVariable String email) {
+            return usuarioRepository.findByEmail(email)
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.notFound().build());
+        }
+
+         // endpoint para modificar el nombre de un usuario
+    @PutMapping("/usuarios/{userId}/nombre")
+    public ResponseEntity<Usuario> modificarNombreUsuario(@PathVariable String userId, @RequestBody String nuevoNombre) {
+        return usuarioRepository.findById(userId)
+            .map(usuario -> {
+                usuario.setNombre(nuevoNombre);
+                Usuario usuarioActualizado = usuarioRepository.save(usuario);
+                return ResponseEntity.ok(usuarioActualizado);
+            })
+            .orElse(ResponseEntity.notFound().build());
     }
 
 
-    // endpoint para obtener un usuario por su email
-    @GetMapping("/usuarios/email/{email}")
-    public ResponseEntity<Usuario> obtenerUsuarioPorEmail(@PathVariable String email) {
-        return usuarioRepository.findByEmail(email)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+
     }
-
-
-
-}
