08a0c3cdec6a1cd1c7fd182bdf6e6b3f1d5ccc84
Felipe Avila <126629999+yazzuke@users.noreply.github.com>
2024-11-18 20:25:35 -0500

    offlinemode

--- src/main/java/co/finanplus/api/controller/CorsConfiguration.java
+++ src/main/java/co/finanplus/api/controller/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://finanplus.netlify.app/")
+                        .allowedOrigins("http://localhost:5173/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true)
